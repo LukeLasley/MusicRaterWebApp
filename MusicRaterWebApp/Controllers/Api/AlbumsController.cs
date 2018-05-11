7b6b28caf42f8586e028b61b046fb2d300e35672
LukeLasley <llasley@comcast.net>
2018-05-11 12:57:11 -0500

    adding api capabilities

---
+++ MusicRaterWebApp/Controllers/Api/AlbumsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicRaterWebApp.Controllers.Api
+{
+    public class AlbumsController : ApiController
+    {
+        private MusicRaterContext _context;
+
+        public AlbumsController()
+        {
+            _context = new MusicRaterContext();
+        }
+    }
+}
