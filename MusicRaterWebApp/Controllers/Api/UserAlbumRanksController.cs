7bbfc6c84171b50fa83c6c5884891ae23e95974d
unknown <llasley@comcast.net>
2018-05-19 14:47:44 -0500

    started put method for victory

--- MusicRaterWebApp/Controllers/Api/UserAlbumRanksController.cs
+++ MusicRaterWebApp/Controllers/Api/UserAlbumRanksController.cs
@@ -24,5 +24,43 @@ namespace MusicRaterWebApp.Controllers.Api
             return userRanks;
         }
 
+        //Put /api/useralbumranks/10
+        [HttpPut]
+        public IHttpActionResult UpdateAlbumRank(int id, double expected)
+        {
+            var databaseAlbumRank = _context.userAlbumRanks.SingleOrDefault(c => c.id == id);
+            if (databaseAlbumRank == null)
+            {
+                return NotFound();
+            }
+            databaseAlbumRank.timesSeen++;
+
+            int k = getKConstant(databaseAlbumRank.timesSeen, databaseAlbumRank.rank);
+            databaseAlbumRank.rank = getNewRank(expected, 1, k, databaseAlbumRank.rank);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        private int getKConstant(int timesSeen, int rank)
+        {
+            if(timesSeen < 10 || rank < 400)
+            {
+                return 32;
+            }
+            if(rank > 500 && rank < 700)
+            {
+                return 24;
+            }
+            else
+            {
+                return 16;
+            }
+        }
+        private int getNewRank(double expected, int result, int K, int rank)
+        {
+            double gainOrLoss = (result - expected) * K;
+            double newRankAsDouble = rank + gainOrLoss;
+            return (int)newRankAsDouble;
+        }
     }
 }
