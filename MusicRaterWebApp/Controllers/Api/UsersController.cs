d352f40b1ad1b2f7601b36ca4918f80102e8e320
LukeLasley <llasley@comcast.net>
2018-06-01 10:02:41 -0500

    search functionality working and only accesable to admin

---
+++ MusicRaterWebApp/Controllers/Api/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MusicRaterWebApp.Controllers.Api
+{
+    public class UsersController : ApiController
+    {
+        private MusicRaterContext _context;
+
+        public UsersController()
+        {
+            _context = new MusicRaterContext();
+        }
+
+        public IHttpActionResult GetUsers()
+        {
+            var users = _context.Users.Where(x => x.UserName != "Admin").Select(x => new { x.UserName, x.Email }).ToList();
+            return Ok(users);
+        }
+
+        public IHttpActionResult GetUser(string searchString)
+        {
+            var users = _context.Users.Where(x => x.UserName != "Admin" && ( x.Email.Contains(searchString) || x.UserName.Contains(searchString))).Select(x => new { x.UserName, x.Email }).ToList();
+            return Ok(users);
+        }
+    }
+}
--- MusicRaterWebApp/Controllers/UserController.cs
+++ MusicRaterWebApp/Controllers/UserController.cs
@@ -29,7 +29,7 @@ namespace MusicRaterWebApp.Controllers
         {
             return View();
         }
-
+        [Authorize(Roles = "Administrator")]
         public ActionResult Search()
         {
             return View();
