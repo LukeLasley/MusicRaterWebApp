1210ec3cb799e21c85734e99e62633ed98511c03
unknown <llasley@comcast.net>
2018-08-02 20:43:42 -0500

    initial structure of user profile

--- MusicRaterWebApp/Controllers/UserController.cs
+++ MusicRaterWebApp/Controllers/UserController.cs
@@ -30,6 +30,37 @@ namespace MusicRaterWebApp.Controllers
             return RedirectToAction("User","Search");
         }
 
+        //TODO: Update this to differentiate from favorites
+        //This view will show the user profile. 
+        [Authorize]
+        public ActionResult UserProfile()
+        {
+            var curUser = User.Identity.GetUserId();
+            var curUserName = User.Identity.GetUserName();
+            //Grabs top ten albumIds, might do well to update the model to already have album object in them so I don't have to make second call.
+            //Albums must have played the rank game at least once to show.
+            var favoriteAlbumIds = _context.userAlbumRanks.Where(x => x.userId.Equals(curUser) && x.knowAlbum == true && x.timesSeen > 0).OrderByDescending(x => x.rank).Select(x => x.albumId).Take(10).ToList();
+            var favoriteAlbumsUnsorted = _context.albums.Where(x => favoriteAlbumIds.Contains(x.albumId)).ToList();
+            var favoriteAlbumsArray = new Album[10];
+            //This takes the unsorted array of favorite albums and sorts them based on the initial albumId query
+            foreach (var album in favoriteAlbumsUnsorted)
+            {
+                int indexOfAlbum = favoriteAlbumIds.IndexOf(album.albumId);
+                favoriteAlbumsArray[indexOfAlbum] = album;
+            }
+            var favoriteAlbums = favoriteAlbumsArray.ToList();
+            //In case the user doesn't have 10 albums, remove the empty albums.
+            favoriteAlbums.RemoveAll(x => x == null);
+            var viewModel = new UserProfileViewModel
+            {
+                username = curUserName,
+                favoriteAlbum = favoriteAlbums
+
+            };
+            return View(viewModel);
+        }
+
+
         //This view will grab the users favorite albums based on rankings
         [Authorize]
         public ActionResult Favorites()
---
+++ MusicRaterWebApp/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MusicRaterWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicRaterWebApp.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public String username { get; set; }
+        public String userId { get; set; }
+        public List<Album> favoriteAlbum { get; set; }
+
+    }
+}
